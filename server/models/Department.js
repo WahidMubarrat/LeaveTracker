5623a82310a2f7a30baaddf9e7ef5553a4df28d8
wmba <163593045+WahidMubarrat@users.noreply.github.com>
2025-12-16 23:40:08 +0600

    Delete server directory

--- server/models/Department.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const departmentSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  headOfDepartment: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // HoD
-  employees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],      // Employee IDs
-});
-
-module.exports = mongoose.model("Department", departmentSchema);
